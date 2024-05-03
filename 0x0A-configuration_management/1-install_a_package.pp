e8c3a326f61b4ec15f9862c3813a30a396f66383
Thabisile <thabisilemkhize@gmail.com>
2024-05-03 14:02:12 -0700

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,12 @@
-# Installing flask
+# Installing a package
 
-class { 'python::pip': }
+package { 'python3-pip':
+  ensure => installed,
+}
 
-package { 'Flask':
-  ensure => present,
-  provider => 'pip',
-  require => Class['python::pip'],
-  version => '2.1.0',
+exec { 'install_flask':
+  command     => '/usr/bin/pip3 install Flask==2.1.0',
+  path        => ['/bin', '/usr/bin/'],
+  environment => 'LC_ALL=C',
+  unless      => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
 }
