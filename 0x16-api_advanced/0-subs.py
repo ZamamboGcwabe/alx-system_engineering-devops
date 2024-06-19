719dd9d2d43c46412d20e9531c4d0294ec7ac58e
Zamambo <122832552+ZamamboGcwabe@users.noreply.github.com>
2024-06-19 09:26:40 +0100

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,27 +1,19 @@
 #!/usr/bin/python3
-""" Queries the Reddit API and returns the number of subscribers.
 """
+Script that queries subscribers on a given Reddit subreddit.
+"""
+
 import requests
 
-def number_of_subscribers(subreddit):
-    """ Return total num of subs."""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'my-app:v1.0.0 (by /u/your_username)'}
-    
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        
-        if response.status_code == 200:
-            data = response.json()
-            return data['data']['subscribers']
-        else:
-            return 0
-    except requests.RequestException:
-        return 0
 
-if __name__ == '__main__':
-    import sys
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
+def number_of_subscribers(subreddit):
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
     else:
-        print("{:d}".format(number_of_subscribers(sys.argv[1])))
+        return 0
